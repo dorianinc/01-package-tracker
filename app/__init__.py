83508ed40aad3e76994a1e1bec7148ffa48f9b0e
Joshua <joshdante@hotmail.com>
2023-04-27 14:02:41 -0400

    almost done with phase 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, url_for, redirect
 from .config import Config
 from .forms.shipping_form import ShippingForm
+from .models import db, Package
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate= Migrate(app, db)
 
 @app.route("/")
 def index():
@@ -12,15 +17,17 @@ def index():
 @app.route("/new_package", methods = ['GET', 'POST'])
 def new_package():
     form = ShippingForm()
-    
+
     if form.validate_on_submit():
-        new_package = {
-            "id": 0,
-            "recipient": form.data["recipient"],
-            "origin": form.data["origin"],
-            "destination": form.data["destination"],
-            "express": form.data["express"]
-        }
-        print(f"new_package 👉 {new_package}")
+        data = form.data
+        new_package = Package(sender=data["sender"],
+                              recipient=data["recipient"],
+                              origin=data["origin"],
+                              destination=data["destination"],
+                              location=data["origin"])
+        print(dir(form.data))
+        db.session.add(new_package)
+        db.session.commit()
+        # print(f"new_package 👉 {new_package}")
         return redirect("/")
     return render_template('shipping_request.html', form=form)
--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     # define any other secret environment variables here
 
 # !!END
