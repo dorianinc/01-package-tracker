9fcd5cf65a7ec1987844a39603eeb386b629aa74
dorianinc <dorianrdba@gmail.com>
2023-04-27 12:51:26 -0700

    completed 01-practice

--- app/__init__.py
+++ app/__init__.py
@@ -8,27 +8,32 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
-migrate= Migrate(app, db)
+migrate = Migrate(app, db)
+
 
 @app.route("/")
 def index():
     packages = Package.query.all()
-    return render_template('package_status.html', packages=packages)
+    return render_template("package_status.html", packages=packages)
 
-@app.route("/new_package", methods = ['GET', 'POST'])
+
+@app.route("/new_package", methods=["GET", "POST"])
 def new_package():
     form = ShippingForm()
 
     if form.validate_on_submit():
         data = form.data
-        new_package = Package(sender=data["sender"],
-                              recipient=data["recipient"],
-                              origin=data["origin"],
-                              destination=data["destination"],
-                              location=data["origin"])
+        new_package = Package(
+            sender=data["sender"],
+            recipient=data["recipient"],
+            origin=data["origin"],
+            destination=data["destination"],
+            location=data["origin"],
+        )
         print(dir(form.data))
         db.session.add(new_package)
         db.session.commit()
         # print(f"new_package 👉 {new_package}")
+        Package.advance_all_locations()
         return redirect("/")
-    return render_template('shipping_request.html', form=form)
+    return render_template("shipping_request.html", form=form)
